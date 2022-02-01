16d41fff6a8ba8182ac6c55575650d4010949b00
EngSalamaNaief <eng.salama.naief@gmail.com>
2022-02-01 18:34:32 +0200

    Signup page from spk editor

---
+++ client/src/pages/SignupPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignupPage (){
+  
+  return(
+    
+    <div className="text-red-200">
+      <div>text</div>
+    </div>
+    )
+}
+
+export default SignupPage;
