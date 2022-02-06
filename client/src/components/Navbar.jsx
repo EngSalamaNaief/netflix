8af3cbefb221659eb799015685409b121f9c4b44
salama naief <eng.salama.naief>
2022-02-06 21:21:51 +0200

    d

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,20 @@ import React,{useState} from "react";
 import {MdSearch,MdNotifications,MdArrowDropDown,MdViewHeadline,MdClose} from "react-icons/md"
 function Navbar(){
   const [dropedDowen,setDropedDowen]=useState(false);
+  const [isScrolled,setIsScrolled]=useState(false);
+
+  window.onscroll =()=>{
+    setIsScrolled(window.scrollY ===0 ? false: true);
+    
+    return ()=>(window.onscroll=null);
+ }
+ console.log(isScrolled)
   return(
-    <div className="text-white bg-transparent h-32 fixed w-full z-40">
+    <div className={`rounded-b-md text-white py-2 fixed w-full z-40`} style={!isScrolled?{background:"linear-gradient(to top,transparent 0%,rgb(0,0,0,0.4) 50%)"}:{background:"black"}}>
       <div className="md:px-10 px-2">
       <div className="flex items-center justify-between">
        <div className=" items-center flex ">
-         <div className="uppercase items-center text-red-500 text-2xl md:text-3xl py-3 pr-8 font-bold cursor-pointer">netflex</div>
+         <div className="uppercase items-center text-red-500 text-2xl md:text-4xl pr-8 font-bold cursor-pointer">netflex</div>
             <div className="md:flex hidden">
              <div className="flex items-center justify-between">
                <div className="capitalize px-2 cursor-pointer">homepage</div>
@@ -22,13 +30,13 @@ function Navbar(){
            <div className="mx-2 text-xl cursor-pointer"><MdSearch/></div>
            <div className="mx-2 cursor-pointer">KID</div>
            <div className="mx-2 text-xl cursor-pointer"><MdNotifications/></div>
-           <div className="w-6 h-8 mx_4 overflow-hidden rounded-full cursor-pointer">
+           <div className="w-6 h-6 mx_4 overflow-hidden rounded-md cursor-pointer">
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPZAaK3gXPnXHy87YFc1UrZ7OdDOC3Rinvw&usqp=CAU" alt="" className="w-full h-full object-cover"/>
            </div>
            <div className="ml-2 relative">
              <MdArrowDropDown onClick={()=>setDropedDowen(!dropedDowen)}  className="text-3xl cursor-pointer"/>
-             <div className={`${dropedDowen?'block':'hidden'} absolute top-10 right-0 p-3 rounded-lg bg-gray-200 text-black`}>
-               <div onClick={()=>setDropedDowen(false)} className="cursor-pointer">Sittings</div>
+             <div className={`${dropedDowen?'block':'hidden'} absolute top-10 right-0 px-4 py-5 rounded-lg bg-black `}>
+               <div onClick={()=>setDropedDowen(false)} className="cursor-pointer mb-1">Sittings</div>
                <div onClick={()=>setDropedDowen(false)} className="pt-2 cursor-pointer">Logout</div>
              </div>
             </div>
@@ -37,7 +45,7 @@ function Navbar(){
          <div className="relative block md:hidden">
           <div onClick={()=>setDropedDowen(!dropedDowen)} className="block md:hidden cursor-pointer text-3xl"> {!dropedDowen?(<MdViewHeadline/>):(<MdClose/>)} </div>
             <div className={`md:hidden w-40 absolute top:40 mt-3 right-0 ${dropedDowen?"block":"hidden"}`}>
-             <div className="bg-gray-200 py-3 px-2 text-black rounded-lg">
+             <div className="bg-black py-3 px-2 rounded-lg">
               <div className="flex mb-4 mt-2 items-center justify-between px-3">
                <div className="w-8 h-8 mx_4 overflow-hidden rounded-full cursor-pointer">
                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPZAaK3gXPnXHy87YFc1UrZ7OdDOC3Rinvw&usqp=CAU" alt="" className="w-full h-full object-cover"/>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-
+import {MdPlayArrow,MdInfoOutline} from "react-icons/md"
 export default function Home() {
   return (
-      <div className="relative">
-           <div className='w-full' style={{height:"90vh"}} >
+      <div className="relative overflow-hidden" style={{height:"90vh"}}>
+           <div className='w-full h-full'  >
               <img src="https://www.thenews.com.pk//assets/uploads/updates/2019-10-10/539290_269048_Avengers_updates.jpg" className='w-full h-full object-cover' alt="" />
            </div>
-           <div className="absolute z-10 bottom-10 left-10">
+           <div className="absolute z-10 bottom-20 left-10 w-full">
              <div className="w-96 h-24">
                <img className="w-full h-full object-fit" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVl-Lx67q85RhM07GTtA-08fIGh2DTVIzDVA&usqp=CAU" alt="" />
              </div>
-             <div className="w-96 my-5 text-lg text-wrap text-white">
-               lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-               lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-               lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+             <div className="w-1/3 leading-6 my-5 text-wrap text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, labore voluptates soluta doloremque eaque quam ea ipsum. Fugit id dignissimos rerum numquam doloremque culpa, odit voluptatum quas! Cupiditate, quod nulla.
              </div>
-             <div>
-              <button className="py-4 px-6 bg-gray-100 mr-3"> play</button>
-              <button className="py-4 px-6 bg-gray-100 mr-3"> info</button>
+             <div className='flex items-center'>
+              <button className="py-1.5 rounded-md px-5 bg-gray-100 mr-3 font-medium flex items-center justify-center"> <MdPlayArrow className='mr-1'/> Play</button>
+              <button className="py-1.5 rounded-md bg-gray-500 text-white px-5 font-medium flex items-center justify-center"><MdInfoOutline className="mr-1"/> Info</button>
               
              </div>
            </div>
